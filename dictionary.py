72b4a84a291c24f1943ddb1d7d26ab3417b91e4e
swayamvikhepatil <swayamvikhe07@gmail.com>
2026-04-21 13:01:24 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+student = {
+ "roll_no": 101,
+ "name": "Amit",
+ "marks": 85
+}
+
+print("Dictionary:", student)
+print("Name:", student["name"])
+print("Marks:", student.get("marks"))
+print()
+
+print("2. Update Dictionary")
+student["marks"] = 90
+student["grade"] = "A"
+print("Updated Dictionary:", student)
+print()
+
+print("3. Removing Elements")
+removed_value = student.pop("grade")
+print("Removed Value:", removed_value)
+print("After Removing 'grade':", student)
+
+student.popitem()
+print("After popitem():", student)
+print()
+
+print("4. Merging Dictionaries")
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+
+merged_dict = dict1 | dict2
+print("First Dictionary:", dict1)
+print("Second Dictionary:", dict2)
+print("Merged Dictionary:", merged_dict)
