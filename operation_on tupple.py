31b3473dfa00a4b79ad98ddd7ef6089765d6b42a
swayamvikhepatil <swayamvikhe07@gmail.com>
2026-04-21 13:00:33 +0530

    Add files via upload

---
+++ operation_on tupple.py
@@ -0,0 +1,9 @@
+tupple=('1','2','3','4')
+print(tupple[0:2])
+print(tupple[3:-1])      
+print(tupple[3]) #index
+print(tupple[-1])#-ing
+print(len(tupple))#length
+print(tupple.count("1"))#count
+tupple.append(2) #dd at end
+print(lists)
